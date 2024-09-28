f0e468f88931c19196c83f617e93da212b5e7c27
= <104793781+gomarcello@users.noreply.github.com>
2024-09-28 00:34:44 -0400

    Update vote_service.go

--- internal/service/vote_service.go
+++ internal/service/vote_service.go
@@ -23,7 +23,9 @@ func NewVoteService() VoteService {
 func (v voteService) Reset(breakout *domain.Breakout) {
 	database.DB.Model(domain.User{}).Where("breakout_id = ?", breakout.ID).Update("vote", "")
 	database.DB.Model(domain.Breakout{}).Where("id = ?", breakout.ID).Update("show_votes", false)
+
 	v.broadcast.ResetVotes(breakout.ID)
+	v.broadcast.Breakout(breakout.ID)
 }
 
 func (v voteService) Vote(user *domain.User, value string) {
@@ -37,6 +39,7 @@ func (v voteService) Vote(user *domain.User, value string) {
 	var breakout domain.Breakout
 	database.DB.Preload("Users", "is_online = ?", true).First(&breakout, "id = ?", user.BreakoutID)
 	database.DB.Model(domain.Breakout{}).Where("id = ?", breakout.ID).Update("show_votes", v.didEveryoneVote(breakout))
+
 	v.broadcast.Breakout(breakout.ID)
 }
 
